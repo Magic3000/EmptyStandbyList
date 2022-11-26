7ee9f4e48364b5a170b4cab0795f4a11ae427882
Magic3000 <vova28032000@gmail.com>
2022-11-26 20:28:10 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿using System.Windows.Forms;
+
+namespace EmptyStandbyList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusBtn = new System.Windows.Forms.Button();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.intervalLbl = new System.Windows.Forms.Label();
+            this.intervalTb = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // statusBtn
+            // 
+            this.statusBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusBtn.Location = new System.Drawing.Point(264, 12);
+            this.statusBtn.Name = "statusBtn";
+            this.statusBtn.Size = new System.Drawing.Size(169, 60);
+            this.statusBtn.TabIndex = 0;
+            this.statusBtn.Text = "status";
+            this.statusBtn.UseVisualStyleBackColor = true;
+            this.statusBtn.Click += new System.EventHandler(this.statusBtn_Click);
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLbl.Location = new System.Drawing.Point(12, 29);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(235, 32);
+            this.statusLbl.TabIndex = 1;
+            this.statusLbl.Text = "Auto-clear status:";
+            // 
+            // intervalLbl
+            // 
+            this.intervalLbl.AutoSize = true;
+            this.intervalLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.intervalLbl.Location = new System.Drawing.Point(40, 96);
+            this.intervalLbl.Name = "intervalLbl";
+            this.intervalLbl.Size = new System.Drawing.Size(207, 32);
+            this.intervalLbl.TabIndex = 2;
+            this.intervalLbl.Text = "Interval (in ms):";
+            // 
+            // intervalTb
+            // 
+            this.intervalTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.intervalTb.Location = new System.Drawing.Point(264, 89);
+            this.intervalTb.Name = "intervalTb";
+            this.intervalTb.Size = new System.Drawing.Size(169, 39);
+            this.intervalTb.TabIndex = 3;
+            this.intervalTb.KeyDown += new System.Windows.Forms.KeyEventHandler(this.intervalTb_KeyDown);
+            this.intervalTb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.intervalTb_KeyPress);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "EmptyStandbyList";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.intervalTb);
+            this.Controls.Add(this.intervalLbl);
+            this.Controls.Add(this.statusLbl);
+            this.Controls.Add(this.statusBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EmptyStandbyList";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button statusBtn;
+        private System.Windows.Forms.Label statusLbl;
+        private System.Windows.Forms.Label intervalLbl;
+        private System.Windows.Forms.TextBox intervalTb;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmptyStandbyList
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 _instance;
+        public Form1()
+        {
+            InitializeComponent();
+            _instance = this;
+            LoadConfig();
+            if (config.enabled)
+                Start();
+            Thread updFrm = new Thread(() =>
+            {
+                while (!_instance.Created)
+                    Thread.Sleep(10);
+                UpdateForm();
+            });
+            updFrm.IsBackground = true;
+            updFrm.Start();
+            
+        }
+
+        public static void UpdateForm()
+        {
+            try
+            {
+                Form1._instance.Invoke(new Action(() => { Form1._instance.intervalTb.Text = config.interval.ToString(); }));
+                Form1._instance.Invoke(new Action(() => { Form1._instance.statusBtn.Text = config.enabled ? "Stop" : "Start"; }));
+            }
+            catch (Exception exc) {MessageBox.Show($"UpdateForm exception: {exc.Message}"); }
+        }
+
+        public static Config config;
+        private static void LoadConfig()
+        {
+            if (!File.Exists(Config.path) || string.IsNullOrWhiteSpace(File.ReadAllText(Config.path)))
+            {
+                File.Create(Config.path).Close();
+                config = new Config();
+            }
+            else
+                config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(Config.path));
+        }
+        private static void SaveConfig()
+        {
+            File.WriteAllText(Config.path, JsonConvert.SerializeObject(config));
+        }
+        public static void Start()
+        {
+            Thread dispatcher = new Thread(() =>
+            {
+                int clearCounter = 0;
+                while (config.enabled)
+                {
+                    ClearMemory.ClearFileSystemCache(true);
+                    clearCounter++;
+                    Form1._instance.Invoke(new Action(() => { Form1._instance.Text = $"EmptyStandbyList ({clearCounter})"; }));
+                    Thread.Sleep(config.interval);
+                }
+            });
+            dispatcher.IsBackground = true;
+            dispatcher.Start();
+        }
+
+        private void statusBtn_Click(object sender, EventArgs e)
+        {
+            config.enabled = !config.enabled;
+            Form1._instance.Invoke(new Action(() => { Form1._instance.statusBtn.Text = config.enabled ? "Stop" : "Start"; }));
+            SaveConfig();
+            if (config.enabled)
+                Start();
+        }
+
+        private void intervalTb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            /*if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }*/
+        }
+
+        private void intervalTb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                var value = int.Parse(_instance.intervalTb.Text);
+                if (value < 1000)
+                {
+                    value = 1000;
+                    Form1._instance.Invoke(new Action(() => { Form1._instance.intervalTb.Text = value.ToString(); }));
+                }
+                config.interval = value;
+                SaveConfig();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon.Visible = false;
+        }
+    }
+    public class Config
+    {
+        public static string path = "config.txt";
+        public bool enabled = false;
+        public int interval = 5000;
+    }
+}
